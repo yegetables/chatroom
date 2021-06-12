70d6191b66812c33057d444e1acc76c341b1ab0d
ajian2002 <ajian020515@163.com>
2021-06-13 00:59:16 +0800

    好友申请查看完成,todo:好友确认同意

--- server/friends.c
+++ server/friends.c
@@ -2,14 +2,3 @@
 #include PROJECT_SERVERHEAD
 extern zlog_category_t* ser;
 extern MYSQL* sql_l;
-int ser_show_friends(info* ms) { return base_GET_MANY_VALUE(ms, 2); }
-
-int ser_show_apply(info* ms) { return base_GET_MANY_VALUE(ms, 2); }
-
-bool ser_add_friend(events* ev)
-{
-    info* ms = &(ev->js);
-    if (!base_sql_query(ms)) return false;  //添加进requests
-    if (!case_IF_DONE(ms)) return false;    // 回复1
-    return false;
-}
