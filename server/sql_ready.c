95a56b977eb2238852fc8e75168920c9f6608ad8
ajian2002 <ajian020515@163.com>
2021-06-22 13:11:26 +0800

    ⚡️

--- server/sql_ready.c
+++ server/sql_ready.c
@@ -186,7 +186,7 @@ bool sql_init_table(MYSQL* sql_l)
                 "CREATE TABLE  IF NOT EXISTS `group` ("
                 "`group_id` int(10) unsigned NOT NULL AUTO_INCREMENT,"
                 "`group_name` varchar(30) NOT NULL,"
-                "PRIMARY KEY (`user_id`)"
+                "PRIMARY KEY (`group_id`)"
                 ") ENGINE=InnoDB AUTO_INCREMENT=50000 DEFAULT CHARSET=utf8");
         if (mysql_query(sql_l, q))
         {
