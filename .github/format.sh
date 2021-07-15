7499363acf5d805b0d3bfee79cc978fabbf866e6
ajian2002 <ajian020515@163.com>
2021-07-15 22:40:21 +0800

    a856564

--- .github/format.sh
+++ .github/format.sh
@@ -4,7 +4,7 @@ set -eu
 REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
 echo "## Initializing git repo..."
 # sudo rm -rf * .*
-git init
+# git init
 echo "### Adding git remote..."
 git remote add origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
 echo "### Getting branch"
