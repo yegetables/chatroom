d86fe8910be57626f4eeb667279eb33b45e7d842
ajian2002 <ajian020515@163.com>
2021-07-15 22:42:03 +0800

    rush b

--- .github/format.sh
+++ .github/format.sh
@@ -6,7 +6,7 @@ echo "## Initializing git repo..."
 # sudo rm -rf * .*
 # git init
 echo "### Adding git remote..."
-git remote add origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
+# git remote add origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
 echo "### Getting branch"
 BRANCH=${GITHUB_REF#*refs/heads/}
 echo "### git fetch $BRANCH ..."
